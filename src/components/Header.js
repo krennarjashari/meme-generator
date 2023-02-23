efd511b99059ce2d2c836110cb0305e13937e091
krennarjashari <62670924+krennarjashari@users.noreply.github.com>
2023-02-23 14:23:18 +0100

    deleted image troll face

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,8 +4,7 @@ import logo from "../images/troll-face.png"
 export default function Header() {
     return (
         <header className="header">
-            <img src={logo} className="header--image" />
-            <h2 className="header--title">Header component</h2>
+            <h2 className="header--title">Meme Generator</h2>
         </header>
     )
 }
