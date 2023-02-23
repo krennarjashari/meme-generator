1f24fb4dfb69e9abf505fee365e48ae503b822a4
krennarjashari <62670924+krennarjashari@users.noreply.github.com>
2023-02-23 23:52:14 +0100

    updating function getMemeImage with useState

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -2,21 +2,25 @@ import React from "react";
 import memesData from "../memesData.js"
 
 export default function Meme() {
-    
-    function getMemeImage(){
-        const memesArray=memesData.data.memes
-        const randomNumber=Math.floor(Math.random()*memesArray.length)
-        const url=memesArray[randomNumber].url
-        console.log(url)
+
+    const [memeImage, setMemeImage]=React.useState("")
+
+    function getMemeImage() {
+        const memesArray = memesData.data.memes
+        const randomNumber = Math.floor(Math.random() * memesArray.length)
+        setMemeImage(memesArray[randomNumber].url)
+        // const url = memesArray[randomNumber].url
+        
     }
 
     return (
         <main>
             <div className="form">
-                <input type="text" placeholder="Top text" className="form-input"/>
-                <input type="text" placeholder="Bottom text" className="form-input"/>
+                <input type="text" placeholder="Top text" className="form-input" />
+                <input type="text" placeholder="Bottom text" className="form-input" />
                 <button className="form-button" onClick={getMemeImage}>Get a new meme image</button>
             </div>
+            <img src={memeImage} className="meme-image"/>
         </main>
     )
 }
