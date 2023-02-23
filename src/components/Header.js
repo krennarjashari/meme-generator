5c209434d603b5cfe81bde8700b822142384ee5e
krennarjashari <62670924+krennarjashari@users.noreply.github.com>
2023-02-23 14:19:24 +0100

    updated header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ export default function Header() {
     return (
         <header className="header">
             <img src={logo} className="header--image" />
-            <h2 className="header--title">Header component</h2>
+            <h2 className="header-title">Header component</h2>
         </header>
     )
 }
