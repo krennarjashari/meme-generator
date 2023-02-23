2a80508941e9d1af0110cd52a8cb2abef3331462
krennarjashari <62670924+krennarjashari@users.noreply.github.com>
2023-02-23 15:31:33 +0100

    creating meme form

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header';
+import Meme from './components/Meme';
 
 function App() {
   return (
-    <Header/>
+    <div>
+      <Header/>
+      <Meme/>
+    </div>
   );
 }
 
---
+++ src/components/Meme.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Meme() {
+    return (
+        <main>
+            <form className="form">
+                <input type="text"/>
+                <input type="text"/>
+                <button>Get a new meme image</button>
+            </form>
+        </main>
+    )
+}
