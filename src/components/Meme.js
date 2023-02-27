02f50dc841f829412cbfb138ee294c8569123b7c
krennarjashari <62670924+krennarjashari@users.noreply.github.com>
2023-02-27 14:13:16 +0100

    css flex improvements

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -3,14 +3,14 @@ import memesData from "../memesData.js"
 
 export default function Meme() {
 
-    const [memeImage, setMemeImage]=React.useState("")
+    const [memeImage, setMemeImage] = React.useState("")
 
     function getMemeImage() {
         const memesArray = memesData.data.memes
         const randomNumber = Math.floor(Math.random() * memesArray.length)
         setMemeImage(memesArray[randomNumber].url)
         // const url = memesArray[randomNumber].url
-        
+
     }
 
     return (
@@ -20,7 +20,9 @@ export default function Meme() {
                 <input type="text" placeholder="Bottom text" className="form-input" />
                 <button className="form-button" onClick={getMemeImage}>Get a new meme image</button>
             </div>
-            <img src={memeImage} className="meme-image"/>
+            <div className="div-image">
+                <img src={memeImage} className="meme-image" />
+            </div>
         </main>
     )
 }
